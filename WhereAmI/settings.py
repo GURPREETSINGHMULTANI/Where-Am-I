2bd96cc4cdb0f7bb6ae60c6c928122af7e50b079
Gurpreet Multani <GURPREETSINGHMULTANI@icloud.com>
2021-09-05 18:46:44 -0400

    Fix Bug in Settings File

--- WhereAmI/settings.py
+++ WhereAmI/settings.py
@@ -127,10 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "/"
