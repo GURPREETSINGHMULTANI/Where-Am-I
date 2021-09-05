2c23ecd6156fdf0858121cd347298fd2204f9d07
Gurpreet Multani <GURPREETSINGHMULTANI@icloud.com>
2021-09-05 19:52:08 -0400

    Fix Bug

--- WhereAmI/settings.py
+++ WhereAmI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['where-am-i-kuriushacks.herokuapp.com']
+ALLOWED_HOSTS = ['where-am-i-kuriushacks.herokuapp.com', '127.0.0.1']
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
